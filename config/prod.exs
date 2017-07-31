e4977c9d48aec6b9a4e9197b6132a1c0dc561a9c
John Mertens <john.mertens@gmail.com>
2017-07-31 04:16:21 -0700

    Fix typo in config var name

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :cable_car_spotter, CableCarSpotter.Endpoint,
   url: [scheme: "https", host: "gentle-headland-25551.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_BASE_KEY")
 
 config :cable_car_spotter, CableCarSpotter.Repo,
   adapter: Ecto.Adapters.Postgres,
