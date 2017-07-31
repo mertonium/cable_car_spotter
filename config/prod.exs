b1a181e385169f6df9fccce2c35daa489aba4a37
John Mertens <john.mertens@gmail.com>
2017-07-31 03:57:26 -0700

    Don't forget your commas

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :cable_car_spotter, CableCarSpotter.Endpoint,
 config :cable_car_spotter, CableCarSpotter.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 10)
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 10),
   ssl: true
 
 # Do not print debug messages in production
