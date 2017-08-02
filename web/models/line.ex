4b9c5a2af85a0387e047219cf2fe31f8cd5f7290
John Mertens <john.mertens@gmail.com>
2017-08-02 06:02:00 -0700

    Add #changeset to Line model
    We'll need it for the tests.

--- web/models/line.ex
+++ web/models/line.ex
@@ -10,4 +10,13 @@ defmodule CableCarSpotter.Line do
   def titles_and_ids(query) do
     from l in query, select: {l.title, l.id}
   end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
 end
