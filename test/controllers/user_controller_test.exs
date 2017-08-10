c80cdab8e522f8a130e95c61c7eb3aa72ba54c8d
John Mertens <john.mertens@gmail.com>
2017-08-10 04:51:11 -0700

    Update user controller tests to work with locales

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -6,18 +6,18 @@ defmodule CableCarSpotter.UserControllerTest do
   @invalid_attrs %{}
 
   test "renders form for new resources", %{conn: conn} do
-    conn = get conn, user_path(conn, :new)
+    conn = get conn, user_path(conn, :new, "en")
     assert html_response(conn, 200) =~ "Register"
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @valid_attrs
-    assert redirected_to(conn) == sighting_path(conn, :index)
+    conn = post conn, user_path(conn, :create, "en"), user: @valid_attrs
+    assert redirected_to(conn) == sighting_path(conn, :index, "en")
     assert Repo.get_by(User, %{email: "j@tubbs.io"})
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @invalid_attrs
+    conn = post conn, user_path(conn, :create, "en"), user: @invalid_attrs
     assert html_response(conn, 200) =~ "Register"
   end
 end
