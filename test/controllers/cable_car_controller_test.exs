6c8416ed82322db02fd20743a2c68c693c44cd6e
John Mertens <john.mertens@gmail.com>
2017-08-11 13:02:14 -0700

    Update cable car controller test to handle locales

--- test/controllers/cable_car_controller_test.exs
+++ test/controllers/cable_car_controller_test.exs
@@ -2,20 +2,20 @@ defmodule CableCarSpotter.CableCarControllerTest do
   use CableCarSpotter.ConnCase
 
   test "lists all entries on index", %{conn: conn} do
-    conn = get conn, cable_car_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing cable cars"
+    conn = get(conn, cable_car_path(conn, :index, "en"))
+    assert html_response(conn, 200) =~ "All Cable Cars"
   end
 
   test "shows chosen resource", %{conn: conn} do
     cable_car = insert_line() |> insert_cable_car(%{ car_number: "No. 123"})
 
-    conn = get conn, cable_car_path(conn, :show, cable_car)
+    conn = get conn, cable_car_path(conn, :show, "en", cable_car)
     assert html_response(conn, 200) =~ "No. 123"
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
-      get conn, cable_car_path(conn, :show, -1)
+      get conn, cable_car_path(conn, :show, "en", -1)
     end
   end
 end
